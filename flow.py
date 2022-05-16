9514003f576c787515de08dcdc3145eee6f3b2fa
asgoshawk <x5553434@gmail.com>
2022-05-17 02:14:44 +0800

    Fix thread issues

--- flow.py
+++ flow.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import filedialog
 import tkinter.messagebox as msg
 from tkinter.constants import CENTER, W, E
+import random
 import settings
 import time, datetime
 import os
@@ -64,14 +65,19 @@ class FlowFrame(tk.Frame):
         # self.borderwidth = 0
         # self.hightlightthickness = 0
         self.configure(bg=settings.YELLOW)
-        # self.flowSensor = FS1012()
 
-        # Thread
+        # Thread (Logging)
         self.isLogging = False
-        self.thread = None
-        self.event = Event()
+        self.loggingThread = None
         self.csv = None
 
+        # Thread (Sensor)
+        self.flowSensor = FS1012()
+        self.flowSensorTP1 = 0
+        self.flowSensorTP2 = 0
+        self.sensorThread = LoopThread(1, self.read_sensor)
+
+        # GUI
         self.panelTitle_lb = tk.Label(text="Flow Sensor", font="Helvetica 18 bold", 
             fg=settings.FG_COLOR, bg=settings.BG_COLOR)
         self.panelTitle_lb.place(x=320+xOffset, y=20+yOffset, anchor=CENTER)
@@ -79,7 +85,7 @@ class FlowFrame(tk.Frame):
         self.sensorIndicator_lb = tk.Label(text="Sensor Status", fg=settings.FG_COLOR, bg=settings.BG_COLOR)
         self.sensorIndicator_cv = tk.Canvas(width=20, height=20, bg=settings.BG_COLOR, borderwidth=0, highlightthickness=0)
         self.sensorFlowRate_lb = tk.Label(text="Flow (mlpm)", fg=settings.FG_COLOR, bg=settings.BG_COLOR)
-        self.sensorFlowRateValue_lb = tk.Label(text="1000", fg=settings.FG_COLOR, bg=settings.BG_COLOR, width=10)
+        self.sensorFlowRateValue_lb = tk.Label(text="0", fg=settings.FG_COLOR, bg=settings.BG_COLOR, width=10)
 
         self.sensorIndicator_lb.place(x=40+xOffset, y=60+yOffset, anchor=W)
         self.sensorIndicator_cv.place(x=160+xOffset, y=60+yOffset, anchor=CENTER)
@@ -101,20 +107,42 @@ class FlowFrame(tk.Frame):
         self.logFileStartStop_btn.place(x=600+xOffset, y=60+yOffset, anchor=E)
         self.logFile_en.place(x=280+xOffset, y=100+yOffset, anchor=W)
 
-        self.update_indicator()
+        # Initialization
+        self.update_indicator(False)
+        self.sensorThread.setDaemon(True)
+        self.sensorThread.start()
+        print("[Sensor frame] The sensor reading starts.")
 
     def check_sensor(self):
-        # return self.flowSensor.check_status
-        return True
+        # return True #if (random.random() > 0.2) else False
+        return self.flowSensor.check_status
 
     def draw_indicator(self, canvasName, color):
         return canvasName.create_oval(1, 1, 19, 19, fill=color, outline="")
 
-    def update_indicator(self):
-        if self.check_sensor():
+    def update_indicator(self, status):
+        if status:
             self.draw_indicator(self.sensorIndicator_cv, settings.GREEN)
         else:
             self.draw_indicator(self.sensorIndicator_cv, settings.RED)
+
+    def read_sensor(self):
+        if self.check_sensor():
+            self.update_indicator(True)
+            self.flowSensorTP1 = self.flowSensor.tp1_value()
+            self.flowSensorTP2 = self.flowSensor.tp2_value()
+            # self.flowSensorTP1 = random.randint(1,2000)     # Fake value
+            # self.flowSensorTP2 = random.randint(1,2000)     # Fake value
+            self.sensorFlowRateValue_lb.config(text=str(self.flowSensorTP1))
+        else:
+            self.update_indicator(False)
+            self.sensorFlowRateValue_lb.config(text="0")
+    
+    def stop_read_sensor(self):
+        if self.sensorThread is not None and self.sensorThread.is_alive():
+            self.sensorThread.stop()
+            print("[Sensor frame] The sensor reading stops.")
+        return self.sensorThread.isStopped()
 
     def select_log_dir(self):
         if self.logFile_en.get() is None:
@@ -145,16 +173,24 @@ class FlowFrame(tk.Frame):
                 timeStamp = datetime.datetime.now().strftime("%Y%m%d_%H-%M-%S")
                 self.csv = open(self.logDir + "/flow_log_" + timeStamp + ".csv", "w")
                 self.write_csv_header()
-                self.thread = LoopThread(self.event, 1, self.record_csv)
-                self.thread.start()
+                self.loggingThread = LoopThread(3, self.record_csv)
+                self.loggingThread.setDaemon(True)
+                self.loggingThread.start()
+                print("[Sensor frame] The logging starts.")
                 return True
             except FileNotFoundError:
                 msg.showerror("Error", "No such file or directory. Please retry.")
                 return False
     
     def stop_logging(self):
-        self.event.set()
-        self.csv.close()
+        if self.loggingThread is not None and self.loggingThread.is_alive():
+            self.loggingThread.stop()
+            if self.csv is not None:
+                self.csv.close()
+            print("[Sensor frame] The logging stops.")
+            return self.loggingThread.isStopped()
+        else:
+            return True
 
     def write_csv_header(self):
         self.csv.write("Time,TP1,TP2,Flow_Rate")
@@ -168,21 +204,36 @@ class FlowFrame(tk.Frame):
             self.write_csv_header()
         
         self.csv.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+",")
-        self.csv.write(str(1)+","+str(0)+","+str(123))
+        self.csv.write(str(self.flowSensorTP1)+","+str(self.flowSensorTP2)+","+str(self.flowSensorTP1 - self.flowSensorTP2))
         self.csv.write("\n")
 
+    def force_stop_threads(self):
+        self.stop_logging()
+        time.sleep(0.5)
+        self.stop_read_sensor()
+        time.sleep(0.5)
+
 class LoopThread(Thread):
-    def __init__(self, event, interval, func, *args):
+    def __init__(self, interval, func, *args):
         Thread.__init__(self)
-        self.event = event
+        self.event = Event()
         self.interval = interval
         self.func = func
         self.args = args
-
+        
     def run(self):
         while not self.event.wait(self.interval - time.time() % self.interval):
             self.func(*self.args)
+            if self.isStopped():
+                return
             time.sleep(0.01)
+    
+    def stop(self):
+        self.event.set()
+    
+    def isStopped(self):
+        return self.event.is_set()
+
 
 def test_sensor():
     fs1012 = FS1012({})
